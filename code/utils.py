d9575f86cd3733af34f04d4364f9adfb71669970
Tara <tobia.fjellman@epfl.ch>
2025-01-06 11:33:09 +0100

    Added <<final>> report results

--- code/utils.py
+++ code/utils.py
@@ -13,6 +13,8 @@ def process_data( data_type = 'df-time-agg', drop_ingeborg = True, keep_coins=10
     info['common_persons'] = p_grouped_filt['person'].unique()
 
     data = pd.read_csv(f'../data/{data_type}.csv')
+    if data_type == 'df-time-agg':
+        data = data[(data['person'] != 'adamF') | (data['coin'] != '0.05EUR') | (data['agg'] != 84)]
     if drop_lonely:
         # remove lone coins and persons
         data = data[~data['coin'].isin(lone_coin['coin'])]
@@ -23,8 +25,6 @@ def process_data( data_type = 'df-time-agg', drop_ingeborg = True, keep_coins=10
     data['heads_tails'] = data['N_start_heads_up'] - data['heads_heads']
     data['same_side'] = data['heads_heads'] + data['tails_tails']
     data['diff_side'] = data['N_throws'] - data['same_side']
-
-
 
     if drop_ingeborg:
         data = data[data['person']!='ingeborgR'] # -> adds both a person and a coin (100 throws, can remove her)
