9a07c180567429a0145cabf67953389095ede62a
Tara <tobia.fjellman@epfl.ch>
2025-01-06 15:16:57 +0100

    Added memory analysis

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,25 @@
-import numpy as np
 import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+
+FACTOR = 1.2
+SMALL_SIZE = 8 * FACTOR
+MEDIUM_SIZE = 11 * FACTOR
+BIGGER_SIZE = 14 * FACTOR
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+#plt.rc('figure', labelsize=SMALL_SIZE)    # fontsize of the x and y labels
+plt.rc('figure',autolayout=True)
+plt.rc('lines', linewidth=2)
+plt.rc('lines', markersize=7)
+
 
 def process_data( data_type = 'df-time-agg', drop_ingeborg = True, keep_coins=10, keep_persons=5, drop_lonely=False ):
     data_agg = pd.read_csv(f'../data/data-agg.csv')
@@ -32,3 +52,37 @@ def process_data( data_type = 'df-time-agg', drop_ingeborg = True, keep_coins=10
     #print(len(info['common_coins']), len(info['common_persons']))
 
     return data, info
+
+
+def deviance_analysis( data, formula, filename, summary=False, force=False ):
+    def fit_model():
+        model = smf.glm(formula=formula, data=data, family=sm.families.Binomial())
+        results = model.fit(disp=True)
+        results.save(f'../models/{filename}.pickle')
+        summ = results.summary()
+        with open(f'../models/{filename}.txt', 'w') as f:
+            fit_history = results.fit_history
+            f.write( f'Iteration:{fit_history['iteration']}\n' )
+            f.write( f'Deviance trajectory:{fit_history['deviance']}\n' )
+            f.write(summ.as_text())
+        return summ, results
+
+    if force :
+        _, results = fit_model()
+    else : 
+        try : 
+            results = sm.load(f'../models/{filename}.pickle')
+        except FileNotFoundError:
+            _, results = fit_model()
+
+    dic = {}
+    dic['formula'] = formula.split('~')[1]
+    dic['deviance'] = results.deviance
+    dic['aic'] = results.aic
+    dic['df_residual'] = results.df_resid
+    dic['df_model'] = results.df_model
+
+    if summary:
+        print(results.summary())
+
+    return dic, results
